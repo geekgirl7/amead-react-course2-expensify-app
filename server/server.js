e33b270e7a68623babd3d731f25cb5dac4171006
popeye <popeye@Cabernet.emz>
2020-03-14 03:56:36 -0500

    Setup production build and server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,16 @@
-const path = require('path'); // as in webpack.config.js
+const path = require('path'); // same as in webpack.config.js
 
 // These 2 lines = "an express application"
 const express = require('express');
 const app = express(); 
 
-const publicPath = path.join(__dirname, '..', 'public');
-
-
 // Now let's customize the express application
 // Serve up the public folder and everything inside:
+const publicPath = path.join(__dirname, '..', 'public');
 app.use(express.static(publicPath));
+
+// read the heroku env variable for the port info OR a specific port on our local machine
+const port = process.env.PORT || 3000;
 
 // Need to process all unhandled requests:
 app.get('*', (req, res) => {
@@ -18,7 +19,7 @@ app.get('*', (req, res) => {
 
 // Start up the server:
 // port 3000 is avail. for dev on all systems = no warnings about using it (for DEV)
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server is up!');
 }); 
 
